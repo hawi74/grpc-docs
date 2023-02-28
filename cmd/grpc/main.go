2243e7713942b125206c6ea317ac697f7b500f4f
Filip Stawicki <filip1.stawicki@coop.no>
2023-02-28 14:36:14 +0100

    Working server

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	context "context"
+	"log"
+	"net"
+
+	"github.com/hawi74/grpc-docs/generated"
+	"google.golang.org/grpc"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+type server struct{
+	generated.UnimplementedTodoServiceServer
+}
+
+// CreateTodo implements generated.TodoServiceServer
+func (*server) CreateTodo(context.Context, *generated.Todo) (*emptypb.Empty, error) {
+	log.Print("Serving /CreateTodo")
+	return &emptypb.Empty{}, nil
+}
+
+// SearchTodo implements generated.TodoServiceServer
+func (*server) SearchTodo(context.Context, *generated.SearchTodoRequest) (*generated.SearchTodoResult, error) {
+	log.Print("Serving /SearchTodo")
+	return &generated.SearchTodoResult{
+		Todos: []*generated.Todo{
+			{
+				Id: "1",
+				Title: "foobar",
+				Done: false,
+			},
+			{
+				Id: "2",
+				Title: "baz",
+				Done:  true,
+			},
+			{
+				Id: "3",
+				Title: "????",
+				Done: false,
+			},
+		},
+		Total: 3,
+		TotalPages: 1,
+	}, nil
+}
+
+var _ generated.TodoServiceServer = (*server)(nil)
 
 func main() {
-    fmt.Println("Hello, world!")
+
+	grpcServer := grpc.NewServer()
+
+	generated.RegisterTodoServiceServer(grpcServer, &server{})
+
+	lis, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatalf("cannot listen: %v", err)
+	}
+
+	log.Print("listening on :8000")
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("cannot serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/hawi74/grpc-docs
 
 go 1.17
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20230227214838-9b19f0bdc514 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
